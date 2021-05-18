0668c482320d85a2702e4b76e8daccd68df301af
Harsh Biyani <harshbiyani07@gmail.com>
2021-05-18 21:29:38 +0530

    Code Refactoring

--- Pig Game/script.js
+++ Pig Game/script.js
@@ -15,7 +15,10 @@ const player1 = document.querySelector('.player--1');
 const player1TotalScore = document.getElementById('score--1');
 const player1CurrentScore = document.getElementById('current--1');
 
-const activePlayer = [true, false];
+// const activePlayer = [true, false];
+
+// can use a varibale to store 0 & 1 to represent which player is active
+let activePlayer = 0;
 
 rollDiceBtn.addEventListener('click', rollDice);
 
@@ -30,8 +33,9 @@ newGameBtn.onclick = () => {
   rollDiceBtn.onclick = rollDice;
   holdBtn.onclick = updateTotalScore;
   // Set Player0 as Starting player
-  activePlayer[0] = true;
-  activePlayer[1] = false;
+  // activePlayer[0] = true;
+  // activePlayer[1] = false;
+  activePlayer = 0;
   player0.classList.add('player--active');
   player1.classList.remove('player--active');
 };
@@ -52,21 +56,25 @@ function switchPlayer() {
 }
 
 function switchActivePlayerToInactive() {
-  if (activePlayer[0]) {
-    player0.classList.remove('player--active');
-    player1.classList.add('player--active');
-    activePlayer[0] = false;
-    activePlayer[1] = true;
-  } else {
-    player1.classList.remove('player--active');
-    player0.classList.add('player--active');
-    activePlayer[1] = false;
-    activePlayer[0] = true;
-  }
+  // if (activePlayer[0]) {
+  //   player0.classList.remove('player--active');
+  //   player1.classList.add('player--active');
+  //   activePlayer[0] = false;
+  //   activePlayer[1] = true;
+  // } else {
+  //   player1.classList.remove('player--active');
+  //   player0.classList.add('player--active');
+  //   activePlayer[1] = false;
+  //   activePlayer[0] = true;
+  // }
+
+  player0.classList.toggle('player--active');
+  player1.classList.toggle('player--active');
+  activePlayer = activePlayer === 0 ? 1 : 0;
 }
 
 function resetCurrentScore() {
-  if (activePlayer[0]) {
+  if (activePlayer === 0) {
     player0CurrentScore.textContent = 0;
   } else {
     player1CurrentScore.textContent = 0;
@@ -74,7 +82,7 @@ function resetCurrentScore() {
 }
 
 function updateCurrentScore(num) {
-  if (activePlayer[0]) {
+  if (activePlayer === 0) {
     player0CurrentScore.textContent =
       Number(player0CurrentScore.textContent) + num;
   } else {
@@ -84,7 +92,7 @@ function updateCurrentScore(num) {
 }
 
 function updateTotalScore() {
-  if (activePlayer[0]) {
+  if (activePlayer === 0) {
     player0TotalScore.textContent =
       Number(player0TotalScore.textContent) +
       Number(player0CurrentScore.textContent);
@@ -94,7 +102,7 @@ function updateTotalScore() {
       Number(player1CurrentScore.textContent);
   }
   if (isGameCompleted()) {
-    const playerName = activePlayer[0] ? 'Player 1' : 'Player 2';
+    const playerName = activePlayer === 0 ? 'Player 1' : 'Player 2';
     alert(`${playerName} has won!!`);
     rollDiceBtn.removeEventListener('click', rollDice);
     holdBtn.removeEventListener('click', updateTotalScore);
@@ -105,8 +113,9 @@ function updateTotalScore() {
 }
 
 function isGameCompleted() {
-  const totalScore = activePlayer[0]
-    ? Number(player0TotalScore.textContent)
-    : Number(player1TotalScore.textContent);
+  const totalScore =
+    activePlayer === 0
+      ? Number(player0TotalScore.textContent)
+      : Number(player1TotalScore.textContent);
   return totalScore >= 100 ? true : false;
 }
