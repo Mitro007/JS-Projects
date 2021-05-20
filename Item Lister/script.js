98be21fb3bb00394a05591b21e552c32ba11b27d
Harsh Biyani <harshbiyani07@gmail.com>
2021-05-20 15:19:04 +0530

    Event Bubbling

--- Item Lister/script.js
+++ Item Lister/script.js
@@ -1,18 +1,20 @@
 'use strict';
 
-const items = document.querySelector('.list-group');
+const itemList = document.getElementById('items');
 const form = document.getElementById('addForm');
 const textField = document.querySelector('.mr-2');
+const btnSearch = document.getElementById('filter');
 
+// Add Item
 form.addEventListener('submit', addItem);
 
-for (let index = 0; index < items.children.length; index++) {
-  items.children[index].children[0].addEventListener('click', removeItem);
-}
+// Remove Item - Event Bubbling
+itemList.addEventListener('click', removeItem);
+
+// Filter Item
+btnSearch.addEventListener('keyup', filterItems);
 
 function addItem(ev) {
-  // If we want to disable the default behavior of the submit (which causes the page to refresh) we need to use the
-  // preventDefault() method of the Event object.
   ev.preventDefault();
 
   // Create Text Element
@@ -22,27 +24,48 @@ function addItem(ev) {
   const btn = document.createElement('button');
   btn.innerHTML = 'X';
   btn.className = 'btn btn-danger btn-sm float-right delete';
-  btn.addEventListener('click', removeItem);
 
   // Create li Element
   const liElement = document.createElement('li');
   liElement.className = 'list-group-item';
   liElement.appendChild(textNode);
   liElement.appendChild(btn);
-  items.appendChild(liElement);
+  itemList.appendChild(liElement);
   textField.value = '';
 }
 
 function removeItem(ev) {
-  let itemName = ev.target.parentNode.textContent;
-  itemName = itemName.slice(0, -1);
+  if (ev.target.classList.contains('delete')) {
+    // console.log(ev.target.parentNode);
+    // console.log(ev.target.parentElement);
+    let itemName = ev.target.parentNode.firstChild.textContent;
 
-  // A boolean indicating whether OK (true) or Cancel (false) was selected.
-  const deleteConfirmation = confirm(
-    `Do you really want to delete ${itemName} ?`
-  );
+    // A boolean indicating whether OK (true) or Cancel (false) was selected.
+    const deleteConfirmation = confirm(
+      `Do you really want to delete ${itemName} ?`
+    );
 
-  if (deleteConfirmation) {
-    items.removeChild(ev.target.parentNode);
+    if (deleteConfirmation) {
+      itemList.removeChild(ev.target.parentNode);
+    }
   }
+}
+
+function filterItems(ev) {
+  const searchedItem = ev.target.value.toLowerCase();
+  // const items = itemList.querySelectorAll('li');
+  // console.log(items);
+  // const itemsArray = Array.from(items);
+  // console.log(itemsArray);
+  const items = Array.from(document.querySelectorAll('li'));
+  items.forEach((item) => {
+    // console.log(item);
+    // console.log(item.firstChild);
+    // console.log(item.firstChild.textContent);
+    if (item.firstChild.textContent.toLowerCase().indexOf(searchedItem) != -1) {
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
 }
