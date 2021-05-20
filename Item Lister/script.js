d40c72c846afd6093310a2ac411ceeaee2ac9fae
Harsh Biyani <harshbiyani07@gmail.com>
2021-05-20 14:12:52 +0530

    Removed unnecessary comments

--- Item Lister/script.js
+++ Item Lister/script.js
@@ -4,21 +4,7 @@ const items = document.querySelector('.list-group');
 const btnSubmit = document.querySelector('.btn-dark');
 const textField = document.querySelector('.mr-2');
 
-// console.log(items);
-// console.log(btnSubmit);
-// console.log(textField);
-console.log(items.children);
-console.log(items.children[0]);
-
-// Returns a collection of an element's child element (excluding text and comment nodes)
-console.log(items.children[0].children);
-
-// Returns a collection of an element's child nodes (including text and comment nodes)
-console.log(items.children[0].childNodes);
-
 btnSubmit.addEventListener('click', (ev) => {
-  // console.log(textField.value);
-
   // If we want to disable the default behavior of the submit (which causes the page to refresh) we need to use the
   // preventDefault() method of the Event object.
   ev.preventDefault();
@@ -31,6 +17,7 @@ btnSubmit.addEventListener('click', (ev) => {
   btn.innerHTML = 'X';
   btn.className = 'btn btn-danger btn-sm float-right delete';
   btn.addEventListener('click', removeItem);
+
   // Create li Element
   const liElement = document.createElement('li');
   liElement.className = 'list-group-item';
@@ -41,19 +28,12 @@ btnSubmit.addEventListener('click', (ev) => {
 });
 
 for (let index = 0; index < items.children.length; index++) {
-  // console.log(items.children[index].children[0]);
   items.children[index].children[0].addEventListener('click', removeItem);
 }
 
 function removeItem(ev) {
-  // console.log(ev.target);
-  // console.log(ev.target.parentNode);
-
   let itemName = ev.target.parentNode.textContent;
-  // console.log(typeof itemName);
-  // console.log(itemName);
   itemName = itemName.slice(0, -1);
-  // console.log(itemName);
 
   // A boolean indicating whether OK (true) or Cancel (false) was selected.
   const deleteConfirmation = confirm(
