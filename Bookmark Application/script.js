9bb5675e959101991ab2d90c0c42eb877b650ff4
Harsh Biyani <Harsh.Biyani@T-Mobile.com>
2021-05-24 09:50:47 +0530

    Adding Bookmark

--- Bookmark Application/script.js
+++ Bookmark Application/script.js
@@ -1,14 +1,24 @@
 'use strict';
 
 const form = document.getElementById('myForm');
+const bookmark = JSON.parse(localStorage.getItem('bookmark')) || [];
+
 form.addEventListener('submit', saveBookmark);
 
 function saveBookmark(ev) {
-  ev.preventDEfault();
-  const siteName = document.getElementById('siteName');
-  console.log(siteName);
-  const siteURL = document.getElementById('siteUrl');
-  console.log(siteURL);
-  // console.log(siteName, siteURL);
-  // localStorage.setItem(siteName, siteURL);
+  const siteName = document.getElementById('siteName').value;
+  const siteURL = document.getElementById('siteUrl').value;
+
+  const isBookmarkPresent =
+    bookmark.length > 0 &&
+    bookmark.some(
+      (element) => element.siteName === siteName && element.siteURL === siteURL
+    );
+
+  if (!isBookmarkPresent) {
+    bookmark.push({ siteName, siteURL });
+    localStorage.setItem('bookmark', JSON.stringify(bookmark));
+  }
+
+  ev.preventDefault();
 }
